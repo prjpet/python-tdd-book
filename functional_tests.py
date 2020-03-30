0c8549f3120337d22b9c8b66241f8f6180f5d51b
Peter Prjevara <peter@os3.nl>
2020-03-30 14:50:35 +0200

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -19,16 +21,24 @@ class NewVisitorTest(unittest.TestCase):
         #Check page title and header mention To-Do
         self.assertIn('To-Do', self.browser.title)
 
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do',header_text)
+        #Enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
+        #Type "Buy peacock feathers" into a text box
+        inputbox.send_keys('Buy peacock feathers')
+        #Hit enter, page update, page lists
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1. Buy peacock feathers' for row in rows))
+        #"1. Buy peacock feathers" as an item in a to-do list
+        
+        #Still a text box inviting to add another item
         self.fail('Finish the test!')
 
-        #Enter a to-do item straight away
-
-        #Type "Buy peacock feathers" into a text box
-        
-        #Hit enter, page update, page lists
-        #"1. Buy peacock feathers" as an item in a to-do list
-
-        #Still a text box inviting to add another item
         # Enter "Use peacock feathers to make a fly"
 
         #Hit enter, page update page lists both items
