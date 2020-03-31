cc026ac8d04bcc2a04c9634b50749ceaa275ea86
Peter Prjevara <peter@os3.nl>
2020-03-31 14:09:01 +0200

    Created helper function to remove duplicate code for checking table items in to-do table.

--- functional_tests.py
+++ functional_tests.py
@@ -13,6 +13,12 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         
 
@@ -30,30 +36,23 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys('Buy peacock feathers')
         #Hit enter, page update, page lists
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(3)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
+        time.sleep(1)
 
         #"1. Buy peacock feathers" as an item in a to-do list
         #new way of checking to-do table contents:
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
         #old way of checking to-do table contents:
         #self.assertTrue(any(row.text == '1. Buy peacock feathers' for row in rows), f"New to-do ite$
 
         #Still a text box inviting to add another item
         inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
-        # Enter "Use peacock feathers to make a fly"
         inputbox.send_keys('Use peacock feathers to make a fly')
-        #Hit enter, page update, page lists
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(3)
+        time.sleep(1)
 
         # The page updates again, and now shows both items on her list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         #Unique URL generated
 
