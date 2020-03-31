b29726a0e2319ba0128567208a288abb887a8288
Peter Prjevara <peter@os3.nl>
2020-03-31 17:37:41 +0200

    Redirect after POST, and show all items in templae

--- lists/views.py
+++ lists/views.py
@@ -1,10 +1,18 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.shortcuts import redirect, render
+from lists.models import Item
 
 # Create your views here.
 
 def home_page(request):
-#    if request.method == 'POST':
-#        return HttpResponse(request.POST['item_text'])
-    return render(request, 'home.html', {'new_item_text': request.POST.get('item_text',''),})
+    if request.method == 'POST':
+        new_item_text = request.POST['item_text']
+        Item.objects.create(text=new_item_text)
+        return redirect('/')
+
+    items = Item.objects.all()
+# long version of creating a new item in db:
+#    item = Item()
+#    item.text = request.POST.get('item_text','')
+#    item.save()
+
+    return render(request, 'home.html', {'items': items})
